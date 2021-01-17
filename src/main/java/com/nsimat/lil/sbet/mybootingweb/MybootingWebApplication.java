243933d5b5e3f9c0875ebe1058d9eed155b68918
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2021-01-17 15:42:13 +0100

    Initial commit:Creating a Hands-on project on Spring Boot, MyBooting-Web.

---
+++ src/main/java/com/nsimat/lil/sbet/mybootingweb/MybootingWebApplication.java
@@ -0,0 +1,13 @@
+package com.nsimat.lil.sbet.mybootingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybootingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybootingWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nsimat/lil/sbet/mybootingweb/MybootingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsimat.lil.sbet.mybootingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybootingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
