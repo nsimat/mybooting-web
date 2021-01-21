f974520972c1a321b8387fc4a2c633c094f37aae
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2021-01-21 17:15:53 +0100

    Refining FizzBuzzApplication.

--- fizz-buzz/src/main/java/com/nsimat/lil/sbet/fizzbuzz/FizzBuzzApplication.java
+++ fizz-buzz/src/main/java/com/nsimat/lil/sbet/fizzbuzz/FizzBuzzApplication.java
@@ -1,7 +1,9 @@
 package com.nsimat.lil.sbet.fizzbuzz;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FizzBuzzApplication {
@@ -10,4 +12,17 @@ public class FizzBuzzApplication {
         SpringApplication.run(FizzBuzzApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner run(){
+        return args -> {
+            for (int i=1; i<=100; i++){
+
+                String result = "";
+                result += (i%3) == 0 ? "Fizz" : "";
+                result += (i%5) == 0 ? "Buzz" : "";
+                System.out.println(!result.isEmpty() ? result : i);
+
+            }
+        };
+    }
 }
