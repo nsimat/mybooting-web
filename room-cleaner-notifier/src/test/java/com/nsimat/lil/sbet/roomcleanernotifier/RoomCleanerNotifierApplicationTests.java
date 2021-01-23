4c655d7338f20c42fdaeadc9606d5c3d6473dc04
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2021-01-23 22:55:12 +0100

    Adding room-cleaner-notifier module to the project.

---
+++ room-cleaner-notifier/src/test/java/com/nsimat/lil/sbet/roomcleanernotifier/RoomCleanerNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsimat.lil.sbet.roomcleanernotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomCleanerNotifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
